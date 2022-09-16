b959c423d2760eb8629a588c2f07d0070be6c365
Tomas Green <tomas.green@tietoevry.com>
2022-09-16 14:17:38 +0200

    Added LBPreviewContainer to better support view state changes.

--- Laiban/Shared/Utilitites/LBPreviewEnvironment.swift
+++ Laiban/Shared/Utilitites/LBPreviewEnvironment.swift
@@ -20,7 +20,7 @@ private let lbPreviewAssistantProxySwitchboard:AudioSwitchboard = {
 private let lbPreviewAssistantTTS:TTS = {
     TTS(AppleTTS(audioSwitchBoard: lbPreviewAssistantProxySwitchboard))
 }()
-private let previewAssistant:Assistant = {
+private func createPreviewAssistant() -> Assistant{
     let a = Assistant(
         sttService: AppleSTT(audioSwitchboard: lbPreviewAssistantProxySwitchboard),
         ttsServices: AppleTTS(audioSwitchBoard: lbPreviewAssistantProxySwitchboard),
@@ -30,11 +30,52 @@ private let previewAssistant:Assistant = {
     a.stt.disabled = true
     a.tts.disabled = true
     return a
+}
+private let previewAssistant:Assistant = {
+    return createPreviewAssistant()
 }()
 private let previewViewState = LBViewState()
 public extension View {
-    func attachPreviewEnvironmentObjects(ttsDisabled:Bool = true) -> some View {
+    func attachPreviewEnvironmentObjects(identity:LBViewIdentity? = nil, ttsDisabled:Bool = true) -> some View {
+        if let identity {
+            previewViewState.navigate(to: identity)
+        }
         previewAssistant.tts.disabled = ttsDisabled
         return self.environmentObject(previewAssistant).environmentObject(previewViewState)
+    }
+}
+struct LBPreviewContainer<Screen:View>: View {
+    @StateObject var assistant = createPreviewAssistant()
+    @StateObject var viewState = LBViewState()
+    var identity:LBViewIdentity
+    var content:() -> Screen
+    public var body: some View {
+        LBFullscreenContainer { props in
+           content()
+        }
+        .onContainerAction { action in
+            switch action {
+            case .back:
+                if viewState.previousValue != nil {
+                    viewState.dismiss()
+                }
+            case .home: viewState.clear()
+            case .languages: viewState.present(.languages)
+            case .admin:  break
+            case .character: break
+            case .custom(let string): print(string)
+            }
+        }
+        .character(image: viewState.characterImage)
+        .character(hidden: viewState.characterHidden)
+        .character(position: viewState.characterPosition)
+        .actionBarButtons(viewState.actionButtons)
+        .animation(.spring(), value: viewState.value)
+        .statusBar(hidden: true)
+        .environmentObject(assistant)
+        .environmentObject(viewState)
+        .onAppear {
+            viewState.navigate(to: identity)
+        }
     }
 }
