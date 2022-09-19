4e31ca2b7bc2344a9db20de158d753ef310f69a9
Tomas Green <tomas.green@tietoevry.com>
2022-09-19 09:24:28 +0200

    Added default implementation to TimeViewContentProvider functions

--- Laiban/Modules/Time/Sources/Views/TimeViewContentProvider.swift
+++ Laiban/Modules/Time/Sources/Views/TimeViewContentProvider.swift
@@ -12,3 +12,11 @@ public protocol TimeViewContentProvider {
     func childrenPublisher() -> AnyPublisher<[TimeViewChildInfo]?,Never>
     func otherClockItemsPublisher() -> AnyPublisher<[ClockItem]?,Never>
 }
+public extension TimeViewContentProvider {
+    func childrenPublisher() -> AnyPublisher<[TimeViewChildInfo]?,Never> {
+        return Just<[TimeViewChildInfo]?>(nil).eraseToAnyPublisher()
+    }
+    func otherClockItemsPublisher() -> AnyPublisher<[ClockItem]?,Never> {
+        return Just<[ClockItem]?>(nil).eraseToAnyPublisher()
+    }
+}
