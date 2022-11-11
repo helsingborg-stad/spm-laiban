3716df66a761f8acaafad45266a744d2a5ecd1b0
Fredrik Häggbom <fredrik.haggbom@promobility.se>
2022-11-11 08:55:12 +0100

    Added test cases for Movement module

--- Laiban/Modules/Movement/Sources/Models/MovementModel.swift
+++ Laiban/Modules/Movement/Sources/Models/MovementModel.swift
@@ -15,15 +15,14 @@ public struct MovementModel: Codable, Equatable {
 }
 
 public struct Movement: Codable,Hashable,Identifiable {
-    public var id:String {
-        return date
-    }
+    public var id:String
     public var date:String
     public var minutes:Int
     public var reported:Bool = false
     public var numMoving:Int = 0
     public var emojis:String = ""
     public init(minutes:Int,date:Date = Date(), numMoving:Int) {
+        self.id = UUID().uuidString
         let f = DateFormatter()
         f.dateFormat = "yyyy-MM-dd"
         self.date = f.string(from: date)
@@ -31,6 +30,7 @@ public struct Movement: Codable,Hashable,Identifiable {
         self.minutes = minutes
     }
     public init(minutes:Int,date:String, numMoving:Int, emojis:String = "") {
+        self.id = UUID().uuidString
         self.date = date
         self.minutes = minutes
         self.numMoving = numMoving
@@ -38,6 +38,7 @@ public struct Movement: Codable,Hashable,Identifiable {
     }
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try values.decode(String.self, forKey: .id) ?? UUID().uuidString
         self.date = try values.decode(String.self, forKey: .date)
         self.minutes = try values.decode(Int.self, forKey: .minutes)
         self.reported = (try? values.decode(Bool.self, forKey: .reported)) ?? false
---
+++ Laiban/Modules/Movement/Tests/MovementTests.swift
@@ -0,0 +1,89 @@
+//
+//  MovementTests.swift
+//  
+//
+//  Created by Fredrik Häggbom on 2022-11-10.
+//
+
+import XCTest
+@testable import Laiban
+
+final class MovementTests: XCTestCase {
+    let service = MovementService()
+    var settingsBackup: MovementSettings?
+    var activitiesBackup: [MovementActivity]?
+    var movementBackup: [Movement]?
+    
+    override func setUpWithError() throws {
+        movementBackup = service.data.movement
+        settingsBackup = service.data.settings
+        activitiesBackup = service.data.activities
+    }
+
+    override func tearDownWithError() throws {
+        if let movementBackup = movementBackup {
+            service.data.movement = movementBackup
+        }
+        if let settingsBackup = settingsBackup {
+            service.data.settings = settingsBackup
+        }
+        if let activitiesBackup = activitiesBackup {
+            service.data.activities = activitiesBackup
+        }
+    }
+
+    func testVerifyInitialData() throws {
+        XCTAssertTrue(service.data.activities.count > 0)
+        XCTAssertTrue(service.data.settings.stepLength > 0)
+        XCTAssertTrue(service.data.settings.maxMetersPerDay > 0)
+        XCTAssertTrue(service.data.settings.stepsPerMinute > 0)
+    }
+    
+    func testAddMovement() throws {
+        let initialMeters = service.movementManager.movementMeters()
+        service.add(Movement(minutes: 60, date: Date(), numMoving: 20))
+        
+        let updatedMeters = service.movementManager.movementMeters()
+        XCTAssertNotEqual(initialMeters, updatedMeters)
+    }
+    
+    func testAddAndRemoveMovement() throws {
+        let initialMeters = service.movementManager.movementMeters()
+        let movement = Movement(minutes: 60, date: Date(), numMoving: 20)
+        service.add(movement)
+        
+        var updatedMeters = service.movementManager.movementMeters()
+        XCTAssertNotEqual(initialMeters, updatedMeters)
+        
+        service.remove(movement)
+        updatedMeters = service.movementManager.movementMeters()
+        XCTAssertEqual(initialMeters, updatedMeters)
+    }
+    
+    func testAddActivity() throws {
+        let initialNumberOfActivities = service.data.activities.count
+        XCTAssertNotEqual(initialNumberOfActivities, 0)
+        let newActivity = MovementActivity(id: UUID().uuidString, colorString: MovementActivity.colorStrings.randomElement()!, title: "Testactivity", emoji: "📶")
+        service.addActivity(newActivity: newActivity, callback: {})
+        
+        var updatedNumberOfActivities = service.data.activities.count
+        XCTAssertEqual(initialNumberOfActivities + 1, updatedNumberOfActivities)
+        
+        XCTAssertNotNil(service.data.activities.first { $0.id == newActivity.id })
+        
+        service.deleteActivity(activity: newActivity, callback: {})
+        updatedNumberOfActivities = service.data.activities.count
+        XCTAssertEqual(initialNumberOfActivities, updatedNumberOfActivities)
+    }
+    
+    func testSettings() throws {
+        service.add(Movement(minutes: 60, date: Date(), numMoving: 20))
+        service.add(Movement(minutes: 30, date: Date(), numMoving: 15))
+
+        let initialMeters = service.movementManager.movementMeters()
+        service.data.settings.stepsPerMinute = service.data.settings.stepsPerMinute / 2
+        let updatedMeters = service.movementManager.movementMeters()
+        print("\(initialMeters), \( updatedMeters)")
+        XCTAssertEqual(initialMeters / 2, updatedMeters)
+    }
+}
