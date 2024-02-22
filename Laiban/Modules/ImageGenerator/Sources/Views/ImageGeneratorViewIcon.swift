7fcd68f74eed9f3d41fda86154e427d75a432afa
MrSpex <mail@dannilsson.se>
2024-02-22 15:09:38 +0100

    feat: add image generator module icon

--- Laiban/Modules/ImageGenerator/Sources/Views/ImageGeneratorViewIcon.swift
+++ Laiban/Modules/ImageGenerator/Sources/Views/ImageGeneratorViewIcon.swift
@@ -1,9 +1,19 @@
 import SwiftUI
 
-public struct ImageGeneratorViewIcon : View {
-    public var body:some View {
-        LBEmojiBadgeView(emoji: "🤖", rimColor: .purple)
+public struct ImageGeneratorViewIcon: View {
+    public var body: some View {
+        LBBadgeView(rimColor: .black) { _ in
+            Image("icon", bundle: .module)
+                .centerCropped()
+                .scaleEffect(CGSize(width: 1.01, height: 1.01))
+        }
     }
 
     public init() {}
+}
+
+struct ImageGeneratorViewIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageGeneratorViewIcon()
+    }
 }
